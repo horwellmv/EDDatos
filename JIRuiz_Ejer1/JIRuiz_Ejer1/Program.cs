57bbfee5a87a76ca7b3aad12e0614d522d196624
Juan Ignacio Ruiz <juanignacioruiz540@gmail.com>
2020-09-05 13:23:59 -0300

    ejerciciofork

---
+++ JIRuiz_Ejer1/JIRuiz_Ejer1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIRuiz_Ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juan Ignacio Ruiz, Ejercicio 1.");
+        }
+    }
+}
